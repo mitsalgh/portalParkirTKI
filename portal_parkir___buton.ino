0cda279878249eda507066e245dc97ae2a2988d1
Aprida55 <124011597+Aprida55@users.noreply.github.com>
2023-02-09 15:18:01 +0700

    Update portal_parkir___buton.ino

--- portal_parkir___buton.ino
+++ portal_parkir___buton.ino
@@ -14,18 +14,46 @@ void setup()
   pinMode(pinButtonOpen, INPUT); 
 }
 
+void prosesOpen()
+{
+//  if(Serial.available()>0)
+//  {
+//    delay(10);
+//    dataSTB= Serial.readString();
+//    Serial.println(
+//    if(dataSTB== "b" || dataSTB== "B" || dataSTB== "b\n" || dataSTB== "B\n" )
+//    {
+      digitalWrite(pinRelayOpen, HIGH);
+      delay(1000);
+      digitalWrite(pinRelayOpen, LOW);
+      Serial.println("opened");
+      delay(3500);
+      digitalWrite(pinRelayClose, HIGH);
+      delay(1000);
+      digitalWrite(pinRelayClose, LOW);
+      delay(3500);
+      Serial.println("Closed");
+//    }
+//  }
+}
+
 void loop() 
 {
+  while(1)
+  {
+    prosesOpen();
+  }
   if(Serial.available()>0) //check Perintah dari STB
   {
     delay(10);
     dataSTB= Serial.readString();
     if(dataSTB== "b" || dataSTB== "B" || dataSTB== "b\n" || dataSTB== "B\n" )
     {
-      digitalWrite(pinRelayOpen, HIGH);
-      delay(1000);
-      digitalWrite(pinRelayOpen, LOW);
-      Serial.println("opened");
+      prosesOpen();
+//      digitalWrite(pinRelayOpen, HIGH);
+//      delay(1000);
+//      digitalWrite(pinRelayOpen, LOW);
+//      Serial.println("opened");
       
     }
     else if (dataSTB== "c" || dataSTB== "C" || dataSTB== "c\n" || dataSTB== "C\n" )
