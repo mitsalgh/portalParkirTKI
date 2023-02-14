8922b146899768a2887db6dc190a1bee19a550f2
dipta01 <124011451+dipta01@users.noreply.github.com>
2023-02-14 11:52:58 +0700

    Update portal_parkir___buton.ino

--- portal_parkir___buton.ino
+++ portal_parkir___buton.ino
@@ -2,6 +2,9 @@
 #define pinRelayClose 3
 #define pinRelayStop 4
 #define pinButtonOpen 5
+#define LEDOpen 6
+#define LEDClose 8
+#define LEDReady 7
 
 String dataSTB="";
 
@@ -11,73 +14,72 @@ void setup()
   pinMode(pinRelayOpen, OUTPUT);
   pinMode(pinRelayClose, OUTPUT);
   pinMode(pinRelayStop, OUTPUT);
-  pinMode(pinButtonOpen, INPUT); 
+  pinMode(LEDOpen, OUTPUT);
+  pinMode(LEDClose, OUTPUT);
+  pinMode(LEDReady, OUTPUT);
+  pinMode(pinButtonOpen, INPUT_PULLUP); 
 }
 
 void prosesOpen()
 {
-//  if(Serial.available()>0)
-//  {
-//    delay(10);
-//    dataSTB= Serial.readString();
-//    Serial.println(
-//    if(dataSTB== "o" || dataSTB== "O" || dataSTB== "o\n" || dataSTB== "O\n" )
-//    {
-      digitalWrite(pinRelayOpen, HIGH);
-      delay(1000);
-      digitalWrite(pinRelayOpen, LOW);
-      Serial.println("opened");
-      delay(3500);
-      digitalWrite(pinRelayClose, HIGH);
-      delay(1000);
-      digitalWrite(pinRelayClose, LOW);
-      delay(3500);
-      Serial.println("Closed");
-//    }
-//  }
+  digitalWrite(pinRelayOpen, HIGH);
+  digitalWrite(LEDReady, LOW);
+  digitalWrite(LEDOpen, HIGH);
+  delay(1000);
+  digitalWrite(pinRelayOpen, LOW);
+  digitalWrite(LEDOpen, LOW);
+  Serial.println("opened");
+  delay(5000);
+  digitalWrite(pinRelayClose, HIGH);
+  digitalWrite(LEDClose, HIGH);
+  digitalWrite(LEDReady,LOW);
+  delay(1000);
+  digitalWrite(pinRelayClose, LOW);
+  digitalWrite(LEDClose, LOW);
+  digitalWrite(LEDReady, HIGH);
+  delay(3500);
+  Serial.println("Closed");
 }
 
 void loop() 
 {
-//  while(1)
-//  {
-//    prosesOpen();
-//  }b
+  digitalWrite(LEDReady,HIGH);
   if(Serial.available()>0) //check Perintah dari STB
   {
     delay(10);
     dataSTB= Serial.readString();
     if(dataSTB== "o" || dataSTB== "O" || dataSTB== "o\n" || dataSTB== "O\n" )
     {
-      prosesOpen();
-//      digitalWrite(pinRelayOpen, HIGH);
-//      delay(1000);
-//      digitalWrite(pinRelayOpen, LOW);
-//      Serial.println("opened");
-      
+      digitalWrite(LEDReady,LOW);
+      digitalWrite(LEDOpen,HIGH);
+      digitalWrite(pinRelayOpen, HIGH);
+      delay(200);
+      digitalWrite(pinRelayOpen, LOW);
+      Serial.println("opened");
+      digitalWrite(LEDOpen,LOW);
     }
     else if (dataSTB== "c" || dataSTB== "C" || dataSTB== "c\n" || dataSTB== "C\n" )
     {
+      digitalWrite(LEDReady,LOW);
+      digitalWrite(LEDClose,HIGH);
       digitalWrite(pinRelayClose, HIGH);
-      delay(1000);
+      digitalWrite(LEDReady,LOW);
+      delay(150);
       digitalWrite(pinRelayClose, LOW);
+      digitalWrite(LEDClose, LOW);
       Serial.println("closed");
-      
     }
     else if (dataSTB== "s" || dataSTB== "S" || dataSTB== "s\n" || dataSTB== "S\n" )
     {
-      digitalWrite(pinRelayStop, HIGH);
+      digitalWrite(pinRelayStop, HIGH);  
       delay(1000);
       digitalWrite(pinRelayStop, LOW);
       Serial.println("stoped");
     }
-    digitalWrite(pinRelayClose, LOW);
-    delay(1000);
-    Serial.println("closed");
   }
-//  else if(digitalRead(pinButtonOpen == LOW))
-//  {
-//    Serial.println("visitor");
-//  }
-
+  else if(digitalRead(pinButtonOpen) == LOW)
+  {
+    Serial.println("visitor");
+    prosesOpen();
+  }
 }
